067e4d6124a8383aecf2f3ddea3a7f4d8c8be8f5
Jack Wilsdon <jack.wilsdon@gmail.com>
2019-02-20 09:02:30 +0000

    Add tests for "use function"

---
+++ test/examples/snippet11.php
@@ -0,0 +1,15 @@
+<?php
+
+use function \first;
+use function second;
+use function \third;
+use function fourth;
+
+class Test
+{
+    public function foo()
+    {
+        first();
+        second();
+    }
+}
