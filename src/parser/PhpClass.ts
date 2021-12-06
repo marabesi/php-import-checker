0001256dab88a708ce14fd013214280c5862fbad
marabesi <matheus.marabesi@gmail.com>
2021-12-06 13:21:27 +0100

    refactor: extracted generic code

--- src/parser/PhpClass.ts
+++ src/parser/PhpClass.ts
@@ -1,20 +1,27 @@
 import { flatMap } from 'lodash';
-import { PhpUseItem, PhpClasses } from '../types/Nodes';
+import { Identifier } from 'php-parser';
+import { PhpClasses } from '../types/Nodes';
 
 export class PhpClass {
 
     constructor(private useTree: any) { }
 
     normalizeClasses(): string[] {
-        const inheritance = this.useTree.map((phpClass: PhpClasses) => {
-            if (phpClass.extends) {
-                return phpClass.extends;
-            }
-            return false;
-        }).filter((item: PhpClasses | Boolean) => item !== false)
-        .map((item: PhpClasses) => item ? item.name : false)
-        .filter((item: PhpClasses | Boolean) => item !== false)
+        const inheritance: string[] = this.extractExtends()
+            .map((item: Identifier) => item ? item.name : '')
+            .filter((item: string) => item !== '')
 
         return inheritance;
+    }
+
+    private extractExtends(): Identifier[] {
+        const extended = this.useTree.map((phpClass: PhpClasses): Identifier[] => {
+            if (phpClass.extends) {
+                return [phpClass.extends];
+            }
+            return [];
+        })
+
+        return flatMap(extended);
     }
 }
