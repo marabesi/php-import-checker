3b079611c0c4366197f708a74eb1d6203121f285
marabesi <matheus.marabesi@gmail.com>
2021-12-12 21:46:25 +0100

    refactor: extracted next version to a file

--- src/core/extractUnusedImports.ts
+++ src/core/extractUnusedImports.ts
@@ -1,47 +1,5 @@
 import { PhpImportCheckerConfiguration } from '../configuration';
-import { Builder } from '../parser/Builder';
-import { PhpUseItem } from '../parser/types/Nodes';
-
-const classUsed = (className: string) => new RegExp('\\b' + className + '\\b', 'g');
-
-function newExtractor(text: string) {
-  let matches = [];
-
-  try {
-    const builder = new Builder(text);
-    const ast:PhpUseItem[] = builder.build().namespaces.normalizeUseStatements();
-
-    const allUsedClasses = ast.map(node => node.name).join(',');
-
-    for (const use of ast) {
-      let found = 0;
-      let splitNameSpace = use.name.split('\\');
-      let className = splitNameSpace[splitNameSpace.length - 1];
-
-      const test = text.match(classUsed(className));
-
-      found = (test || []).length;
-      const classesInNamespace = allUsedClasses.match(classUsed(className)) || [];
-
-      if (found <= classesInNamespace.length) {
-        matches.push({
-          classname: className,
-          found: found,
-          match: {
-            index: use?.loc?.start.offset ?? 0,
-            0: {
-              length: (use?.loc?.end.column ?? 0) - 2,
-            }
-          },
-        });
-      }
-    }
-  } catch (e) {
-    console.log(e);
-  }
-
-  return matches;
-}
+import { newExtractor } from './nextVersionExtractUnusedImports';
 
 export function extractUnusedImports(text: string, configuration: PhpImportCheckerConfiguration) {
   if (configuration.use_next_version) {
---
+++ src/core/nextVersionExtractUnusedImports.ts
@@ -0,0 +1,43 @@
+import { Builder } from '../parser/Builder';
+import { PhpUseItem } from '../parser/types/Nodes';
+
+export const classUsed = (className: string) => new RegExp('\\b' + className + '\\b', 'g');
+
+export function newExtractor(text: string) {
+  let matches = [];
+
+  try {
+    const builder = new Builder(text);
+    const ast:PhpUseItem[] = builder.build().namespaces.normalizeUseStatements();
+
+    const allUsedClasses = ast.map(node => node.name).join(',');
+
+    for (const use of ast) {
+      let found = 0;
+      let splitNameSpace = use.name.split('\\');
+      let className = splitNameSpace[splitNameSpace.length - 1];
+
+      const test = text.match(classUsed(className));
+
+      found = (test || []).length;
+      const classesInNamespace = allUsedClasses.match(classUsed(className)) || [];
+
+      if (found <= classesInNamespace.length) {
+        matches.push({
+          classname: className,
+          found: found,
+          match: {
+            index: use?.loc?.start.offset ?? 0,
+            0: {
+              length: (use?.loc?.end.column ?? 0) - 2,
+            }
+          },
+        });
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  return matches;
+}
