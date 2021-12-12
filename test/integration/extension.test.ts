660eec729d7499be96956231433474c9b0e8d97c
marabesi <matheus.marabesi@gmail.com>
2021-12-12 15:19:34 +0100

    fix: integration test for next version

--- test/integration/extension.test.ts
+++ test/integration/extension.test.ts
@@ -7,22 +7,42 @@ import * as myExtension from '../../src/extension';
 const testFolderLocation = '/../../../test/examples/'
 
 suite('php-import-checker extension behavior', () => {
+    suite('handle php with valid syntax', () => {
+        [
+            ...dataProvider,
+        ].forEach((testCase) => {
+            test('Should identify when there is no used class in a text, snippet:::' + testCase.snippet, async () => {
+                const uri = vscode.Uri.file(
+                    path.join(__dirname + testFolderLocation + testCase.snippet)
+                );
 
-    [
-        ...dataProvider,
-        ...dataProviderNextVersion,
-    ].forEach((testCase) => {
-        test('Should identify when there is no used class in a text, snippet:::' + testCase.snippet, async () => {
-            const uri = vscode.Uri.file(
-                path.join(__dirname + testFolderLocation + testCase.snippet)
-            );
+                const document = await vscode.workspace.openTextDocument(uri);
+                const editor = await vscode.window.showTextDocument(document);
 
-            const document = await vscode.workspace.openTextDocument(uri);
-            const editor = await vscode.window.showTextDocument(document);
+                const found = myExtension.findMatch(editor, editor.document.getText(), {});
 
-            const found = myExtension.findMatch(editor, editor.document.getText(), {});
+                assert.equal(testCase.unused, found.length);
+            });
+        });
+    });
 
-            assert.equal(testCase.unused, found.length);
+    suite('use_next_version: handle php with valid syntax', () => {
+        [
+            ...dataProvider,
+            ...dataProviderNextVersion,
+        ].forEach((testCase) => {
+            test('snippet:::' + testCase.snippet, async () => {
+                const uri = vscode.Uri.file(
+                    path.join(__dirname + testFolderLocation + testCase.snippet)
+                );
+
+                const document = await vscode.workspace.openTextDocument(uri);
+                const editor = await vscode.window.showTextDocument(document);
+
+                const found = myExtension.findMatch(editor, editor.document.getText(), { use_next_version: true });
+
+                assert.equal(testCase.unused, found.length);
+            });
         });
     });
 });
