e92a92ff94f3a0e649b04e1751113a25ebb109bd
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-03-05 15:24:11 +0100

    chore: removes old php import checker code

--- src/core/extractUnusedImports.ts
+++ src/core/extractUnusedImports.ts
@@ -1,11 +1,6 @@
 import { PhpImportCheckerConfiguration } from '../configuration';
 import { newExtractor } from './nextVersionExtractUnusedImports';
-import { oldExtractor } from './oldExtractor';
 
 export function extractUnusedImports(text: string, configuration: PhpImportCheckerConfiguration) {
-  // if (configuration.use_next_version) {
     return newExtractor(text, configuration);
-  // }
-
-  // return oldExtractor(text);
 }
