4e3669ad0fe3cdfac26bd3ac80523919483e2696
Matheus Marabesi <matheus.marabesi@gmail.com>
2023-02-25 20:47:37 +0100

    chore: adds test cases related to intersection types
    refs #481

---
+++ test/examples/snippet44.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Project;
+
+use App\Models\Bar1;
+use App\Models\Bar2;
+use App\Models\Bar3;
+
+class Foo
+{
+    public function __construct(array | Bar2 $bar = null)
+    {
+    }
+}
